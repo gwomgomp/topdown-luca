c602c816de385445e9746fd56d0e2264a79a9298
Luca Neukom <luca.neukom@gmail.com>
2021-09-17 11:57:44 +0200

    add resetting and camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,39 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     GameObject car;
+    [SerializeField]
+    GameObject cameraContainer;
 
     private Rigidbody carRigidBody;
     private Collider carCollider;
     private CarData carData;
     private Transform carBody;
 
-    private static float tiltPower = 10;
+    private float resetTimer = 0;
+    private float defaultCameraHeight;
+
+
+    [SerializeField]
+    private float tiltPower = 10;
+    
+    [SerializeField]
+    private float resetCooldown = 5;
+    
+    [SerializeField]
+    private float maxZoomIncrease = 20;
+    
+    [SerializeField]
+    private float zoomSpeed = 5;
+    
+    [SerializeField]
+    private float cameraSpeed = 5;
+    [SerializeField]
+    private float lookAhead = 5;
 
     void Start()
     {
         carRigidBody = car.GetComponent<Rigidbody>();
         carData = car.GetComponent<CarData>();
         carBody = car.transform.Find("Body").transform;
+        defaultCameraHeight = cameraContainer.transform.position.y;
     }
 
     void Update()
     {
+        Vector3 driveDirection = new Vector3(carRigidBody.velocity.x, 0, carRigidBody.velocity.z); // disregard vertical movement
+        float percentageMaxSpeed = Mathf.Clamp01(driveDirection.magnitude / carData.MaxSpeed);
         if (IsCarGrounded()) {
             HandleJumping();
             HandleAcceleration();
-            HandleTurning();
+            HandleTurning(percentageMaxSpeed);
         }
+        UpdateCamera(percentageMaxSpeed);
+        HandleReset(percentageMaxSpeed);
     }
 
     bool IsCarGrounded() {
         Vector3 raycastOrigin = car.transform.position;
         raycastOrigin.y += 0.5f;
-        return Physics.Raycast(raycastOrigin, Vector3.down, 1);
+        return Physics.Raycast(raycastOrigin, car.transform.up * -1, 1);
     }
 
     void HandleJumping() {
@@ -54,9 +78,8 @@ public class PlayerController : MonoBehaviour
         carRigidBody.velocity = Vector3.ClampMagnitude(carRigidBody.velocity, carData.MaxSpeed);
     }
 
-    void HandleTurning() {
+    void HandleTurning(float percentageMaxSpeed) {
         float steering = Input.GetAxis("Horizontal");
-        float percentageMaxSpeed = carRigidBody.velocity.magnitude / carData.MaxSpeed;
         if (percentageMaxSpeed > 0.1) {
             float forward = Mathf.Sign(Vector3.Dot(car.transform.forward, carRigidBody.velocity.normalized));
             car.transform.Rotate(car.transform.up, steering * carData.TurnRate * Time.deltaTime * forward);
@@ -66,5 +89,32 @@ public class PlayerController : MonoBehaviour
             carBody.localEulerAngles.y,
             carBody.localEulerAngles.z
         );
+    }
+
+    /* the position of cameraContainer is always above the player
+       the position of the actual camera is used for relative movement like lookahead and zoom
+    */
+    void UpdateCamera(float percentageMaxSpeed) {
+        cameraContainer.transform.position = new Vector3(
+            car.transform.position.x,
+            defaultCameraHeight, // gotta adjust this if the track height ever changes
+            car.transform.position.z
+        );
+        Vector3 lookAheadPosition = carRigidBody.velocity.normalized * lookAhead * percentageMaxSpeed;
+        Vector3 newCameraPosition = Vector3.MoveTowards(Camera.main.transform.localPosition, lookAheadPosition, Time.deltaTime * cameraSpeed);
+        float newCameraHeight = maxZoomIncrease * percentageMaxSpeed;
+        newCameraPosition.y = Mathf.MoveTowards(Camera.main.transform.localPosition.y, newCameraHeight, Time.deltaTime * zoomSpeed);
+        Camera.main.transform.localPosition = newCameraPosition;
+    }
+
+    void HandleReset(float percentageMaxSpeed) {
+        if (percentageMaxSpeed <= 0.1 && resetTimer <= 0 && Input.GetButtonDown("Reset")) {
+            Vector3 newPosition = car.transform.position + new Vector3(0, 5, 0);
+            Quaternion newRotation = Quaternion.Euler(0, car.transform.localEulerAngles.y, 0);
+            car.transform.SetPositionAndRotation(newPosition, newRotation);
+            resetTimer = resetCooldown;
+        } else if (resetTimer > 0) {
+            resetTimer -= Time.deltaTime;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Top-Down-Racer Project
 
+## Controls
+
+- W for acceleration
+- S for reverse acceleration
+- A and D for turning
+- X for breaking
+- Space for jumping
+- R to reset car
+
 ## Finished and potential features
+
 - [x] Car movement
-- [ ] Camera movement
+- [x] Camera movement
 - [ ] Game Logic
   - [ ] Laps
   - [ ] Timer
