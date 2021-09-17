21ce2f0c5de032b09ba1a9a5fcaacb591e24432c
Luca Neukom <luca.neukom@gmail.com>
2021-09-17 13:42:12 +0200

    add safe zone configuration to ObstacleSpawner

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -10,14 +10,16 @@ public class ObstacleSpawner : MonoBehaviour
     internal int Count { get; private set; }
     [field: SerializeField]
     internal float Distance { get; private set; }
+    [field: SerializeField]
+    internal float SafeZone { get; private set; }
 
     void Start() {
         for (int i = 0; i < Count; i++) {
             float x = Random.Range(-Distance, Distance);
             float z = Random.Range(-Distance, Distance);
             Vector3 position = new Vector3(x, 0, z);
-            if (position.magnitude <= 10) {
-                position = position.normalized * 10;
+            if (position.magnitude <= SafeZone) {
+                position = position.normalized * SafeZone;
             }
             position.y += 5;
             Instantiate(Obstacle, position, Random.rotationUniform);
